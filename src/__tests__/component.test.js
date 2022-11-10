ece3a31644fe7caf1d1dc4828a2ea366531ea12c
shella12 <ayeshaarshad4567@gmail.com>
2022-11-10 21:50:54 +0500

    Fix linters

--- src/__tests__/component.test.js
+++ src/__tests__/component.test.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-import { Link, MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import Home from '../component/home';
+import Calculator from '../component/calculator';
+import Quote from '../component/quote';
 
 describe('test suite for components', () => {
-    it('renders correctly home component', () => {
-        const tree = renderer
-        .create(<MemoryRouter><Link page="http://localhost:3000/">Home</Link></MemoryRouter>)
-        .toJSON();
-        expect(tree).toMatchSnapshot();
-    });
-    it('renders correctly calculator component', () => {
-        const tree = renderer
-        .create(<MemoryRouter><Link page="http://localhost:3000/calculator">Calculator</Link></MemoryRouter>)
-        .toJSON();
-        expect(tree).toMatchSnapshot();
-    });
-    it('renders correctly quote component', () => {
-        const tree = renderer
-        .create(<MemoryRouter><Link page="http://localhost:3000/quote">Quote</Link></MemoryRouter>)
-        .toJSON();
-        expect(tree).toMatchSnapshot();
-    });
-})
+  it('renders correctly home component', () => {
+    const tree = render(<Home />);
+    expect(tree).toMatchSnapshot();
+  });
+  it('renders correctly calculator component', () => {
+    const tree = render(<Calculator />);
+    expect(tree).toMatchSnapshot();
+  });
+  it('renders correctly quote component', () => {
+    const tree = render(<Quote />);
+    expect(tree).toMatchSnapshot();
+  });
+});
